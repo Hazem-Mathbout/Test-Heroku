19d281b6531c6349ed4258b431a95fb7b3cbb9c7
Hazem-Mathbout <hazemmathbout93@gmail.com>
2022-08-08 19:23:59 +0300

    second commit

--- greeting.py
+++ greeting.py
@@ -106,6 +106,51 @@ def scrapKhamsat(num:int):
     return (finalRes)
 
 
+@app.route("/resMost", methods = ["POST" , "GET"])
+def scrapmostaql(budget_max = 10000, budget_min= 0.00, num_bage = 1):
+    finalRes = {}
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--proxy-server='direct://'")
+    chrome_options.add_argument("--proxy-bypass-list=*")
+    chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--ignore-certificate-errors')
+    chrome_options.add_argument('--allow-running-insecure-content')
+    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36'
+    chrome_options.add_argument(f'user-agent={user_agent}')
+    
+    # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()) , chrome_options=chrome_options)
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    driver.implicitly_wait(10)
+    driver.maximize_window()
+    # url = 'https://mostaql.com/projects?budget_max=10000&sort=latest'
+    
+    url = f"https://mostaql.com/projects?page={num_bage}&budget_min={budget_min}&budget_max={budget_max}&sort=latest"
+    
+    driver.get(url)
+    
+    listResult = []
+    results = driver.find_elements(by= By.CLASS_NAME ,value= "project-row")
+    for res in results:
+        title = res.find_element(by= By.XPATH, value= './td/div[1]/div[1]/h2/a').text 
+        url = res.find_element(by= By.XPATH, value= './td/div[1]/div[1]/h2/a').get_attribute('href')
+        time = res.find_element(by=By.XPATH, value= './td/div[1]/div[1]/ul/li[2]/time ').text                                                                                  
+        listResult.append({"title" : title , "url" : url , "time" : time})    
+    
+    finalRes = json.dumps(listResult)
+    return (finalRes)
+    
+
+
+
+
+
 @app.route('/')
 def index():
     # A welcome message to test our server
