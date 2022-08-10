6eb5b6ebf7d7f73dfeb525943c08b6de7ea305f3
Hazem-Mathbout <hazemmathbout93@gmail.com>
2022-08-10 22:17:47 +0300

    third commit

--- scrap-mostaql.py
+++ scrap-mostaql.py
@@ -1,55 +1,76 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.common.by import By
 
-finalRes = {}
-num_bage = 1
-budget_min= 0.00
-budget_max = 10000
+# finalRes = {}
+# num_bage = 1
+# budget_min= 0.00
+# budget_max = 10000
 
-chrome_options = webdriver.ChromeOptions()
+# chrome_options = webdriver.ChromeOptions()
 
-chrome_options.add_argument("--window-size=1920,1080")
-chrome_options.add_argument("--disable-extensions")
-chrome_options.add_argument("--proxy-server='direct://'")
-chrome_options.add_argument("--proxy-bypass-list=*")
-chrome_options.add_argument("--start-maximized")
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--ignore-certificate-errors')
-chrome_options.add_argument('--allow-running-insecure-content')
-user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36'
-chrome_options.add_argument(f'user-agent={user_agent}')
+# chrome_options.add_argument("--window-size=1920,1080")
+# chrome_options.add_argument("--disable-extensions")
+# chrome_options.add_argument("--proxy-server='direct://'")
+# chrome_options.add_argument("--proxy-bypass-list=*")
+# chrome_options.add_argument("--start-maximized")
+# chrome_options.add_argument('--headless')
+# chrome_options.add_argument('--disable-gpu')
+# chrome_options.add_argument('--disable-dev-shm-usage')
+# chrome_options.add_argument('--no-sandbox')
+# chrome_options.add_argument('--ignore-certificate-errors')
+# chrome_options.add_argument('--allow-running-insecure-content')
+# user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36'
+# chrome_options.add_argument(f'user-agent={user_agent}')
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()) , chrome_options=chrome_options)
+# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()) , chrome_options=chrome_options)
 
-driver.implicitly_wait(10)
-driver.maximize_window()
+# driver.implicitly_wait(10)
+# driver.maximize_window()
 
-url = f"https://kafiil.com/kafiil/public/projects?page=1&source=web"
+# url = f"https://kafiil.com/kafiil/public/projects?page=1&source=web"
 
-driver.get(url)
+# driver.get(url)
 
-listResult = []
-results = driver.find_elements(by=By.CLASS_NAME, value='project-box')
-print("=================")
-print(len(results))
-print("=================")
+# listResult = []
+# results = driver.find_elements(by=By.CLASS_NAME, value='project-box')
+# print("=================")
+# print(len(results))
+# print("=================")
 
-for res in results:    
-    title = res.find_element(by= By.XPATH, value= './div[1]/div[1]/div/a').text.split(' ', 1)[1]                                                                                                           
-    # url = res.find_element(by= By.XPATH, value= './div[1]/div[1]/div/a').get_attribute('href')
-    # time = res.find_element(by=By.XPATH, value= './div[1]/div[1]/div/div/span[1]').text
-    # status = res.find_element(by=By.XPATH, value= './div[1]/div[1]/div/a/span').text  
-    # price = res.find_element(by=By.XPATH, value= './div[1]/div[2]/p').text 
-    # number_of_offers = res.find_element(by=By.XPATH, value= './div[1]/div[1]/div/div/span[2]').text 
-    # url_img = res.find_element(by=By.XPATH, value= './td[1]/a/img').get_attribute('src')
-    # listResult.append({"title" : title , "url" : url , "time" : time}) 
-    print(title)
+# for res in results:    
+#     title = res.find_element(by= By.XPATH, value= './div[1]/div[1]/div/a').text.split(' ', 1)[1]                                                                                                           
+#     # url = res.find_element(by= By.XPATH, value= './div[1]/div[1]/div/a').get_attribute('href')
+#     # time = res.find_element(by=By.XPATH, value= './div[1]/div[1]/div/div/span[1]').text
+#     # status = res.find_element(by=By.XPATH, value= './div[1]/div[1]/div/a/span').text  
+#     # price = res.find_element(by=By.XPATH, value= './div[1]/div[2]/p').text 
+#     # number_of_offers = res.find_element(by=By.XPATH, value= './div[1]/div[1]/div/div/span[2]').text 
+#     # url_img = res.find_element(by=By.XPATH, value= './td[1]/a/img').get_attribute('src')
+#     # listResult.append({"title" : title , "url" : url , "time" : time}) 
+#     print(title)
 
-# print(listResult)   
+# # print(listResult)   
 
 
+
+# Python3 code to demonstrate working of
+# Removing Initial word from string
+# Using split() and join()
+ 
+# initializing string
+test_str = "مفتوح كتابة محتوى على موقع ووردبريس باللغة العربية"
+ 
+# printing original string
+print("The original string is : " + test_str)
+ 
+# Using split() and join()
+# Removing Initial word from string
+x = test_str.split()
+print(x)
+if(x[0] == "قيد"):
+    res=" ".join(x[2:])
+else:
+    res = " ".join(x[1:]) 
+# printing result
+print("The string after omitting first word is : " + str(res))
